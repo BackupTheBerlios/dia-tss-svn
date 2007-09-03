cfb012d393de7a59b8287998b6c865eee080f115
olzzen <olzzen@9ede6eb1-6a10-0410-a49c-957239f13d8d>
2007-09-03 16:01:48 +0000

    lizenzinformationen hinzugefügt

--- trunk/include/plugins/hough_transform.hpp
+++ trunk/include/plugins/hough_transform.hpp
@@ -1,15 +1,28 @@
-/*
- * C++ implementation of the Hough-Transformation
- *
- * Autor:   Daniel Esser, Oliver Kriehn
- * Date:    13.02.2007
-*/
+#   Copyright (C) 2006  Daniel Esser, Oliver Kriehn
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
 #ifndef HOUGH_TRANSFORM
 #define HOUGH_TRANSFORM
 
 #include "gamera.hpp"
 #include <cmath>
 
+/*
+ * C++ implementation of the Hough-Transformation
+*/
 namespace Gamera {
 
     Image* hough_transform( ImageVector &imgVec, PyObject* resultList,
